efa162005d5cb2ce289b3e9bb2cdfc1fae5adc9c
Jason Maurer <jason@maur.co>
2020-04-21 19:41:30 -0600

    Code from guided lecture

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,24 +3,69 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
-
+router.get("/", async (req, res, next) => {
+	try {
+		// translates to `SELECT * FROM "messages";`
+		const messages = await db.select("*").from("messages")
+		res.json(messages)
+	} catch (err) {
+		next(err)
+	}
 })
 
-router.get("/:id", (req, res, next) => {
-
+router.get("/:id", async (req, res, next) => {
+	try {
+		// SELECT * FROM "messages" WHERE "id" = ? LIMIT 1;
+		// const [message] = await db.select("*").from("messages").where("id", req.params.id).limit(1)
+		const message = await db("messages").where("id", req.params.id).first()
+		res.json(message)
+	} catch (err) {
+		next(err)
+	}
 })
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+	try {
+		const payload = {
+			title: req.body.title,
+			contents: req.body.contents,
+		}
 
+		// translates to `INSERT INTO "messages" ("title", "contents") VALUES (?, ?);`
+		const [id] = await db("messages").insert(payload)
+		const message = await db("messages").where("id", id).first()
+
+		res.json(message)
+	} catch (err) {
+		next(err)
+	}
 })
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
+	try {
+		const payload = {
+			title: req.body.title,
+			contents: req.body.contents,
+		}
 
+		// translates to `UPDATE "messages" SET "title" = ? AND "contents" = ? WHERE "id" = ?;`
+		await db("messages").where("id", req.params.id).update(payload)
+		const updatedMessage = await db("messages").where("id", req.params.id).first()
+
+		res.json(updatedMessage)
+	} catch (err) {
+		next(err)
+	}
 })
 
-router.delete("/:id", (req, res, next) => {
-
+router.delete("/:id", async (req, res, next) => {
+	try {
+		// translates to `DELETE FROM "messages" WHERE "id" = ?;`
+		await db("messages").where("id", req.params.id).del()
+		res.status(204).end()
+	} catch (err) {
+		next(err)
+	}
 })
 
 module.exports = router
