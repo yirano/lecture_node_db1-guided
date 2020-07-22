694a34c881264ea2f37bc5f5384ee85a7eee64fa
yirano <yirano@protonmail.ch>
2020-07-21 23:16:22 -0400

    Completed Lecture

--- README.md
+++ README.md
@@ -13,3 +13,48 @@ In this project we will cover the basics of `Structure Query Language (SQL)`, `R
 - [ ] fork and clone this repository.
 - [ ] **CD into the folder** where you cloned **your fork**.
 - [ ] type `npm install` to download dependencies.
+
+
+## Lecture Notes
+
+Relational Databases - similar to spreadsheets (Spreadsheets on steroids)
+
+Table - Collection of many rows
+
+Row - A single record in that table
+
+Column - A value required for each row
+
+---
+
+### Structured Query Language (SQL)
+```sql
+SELECT <columns> FROM <table> WHERE <column> = <value>
+-- is -- 
+SELECT email 
+FROM people 
+WHERE name = 'Corbin King';
+```
+- It's convention to uppercase SQL commands
+- Don't forget the semi-colon -- that ends the command
+
+### Sorting (ORDER BY)
+
+Ascending sort (ASC): a/b/c/d and 1/2/3/4
+
+Descending sort (DESC): z/y/x/w and 4/3/2/1
+
+---
+### Types of Data Language
+- Data Query Language (DQL) - Commands asking questions about the data
+- Data Manipulation Language (DML) - Commands are creating, updating, and deleting data.
+- Data Definition Language (DDL) - Commands for defining the database structure
+
+---
+
+### Data Manipulation Language (DML)
+
+```sql
+INSERT INTO <table> (<columns>) VALUES (<values>);
+UPDATE INTO <table> SET <column> = <value> WHERE <column> = <value>;
+DELETE FROM <table> WHERE <column> = <value>
--- messages/messages-router.js
+++ messages/messages-router.js
@@ -1,83 +1,85 @@
-const express = require("express")
-const db = require("../data/config")
+const express = require('express')
+const db = require('../data/config')
 
 const router = express.Router()
-
-router.get("/", async (req, res, next) => {
-	try {
-		// translates to `SELECT * FROM "messages";`
-		const messages = await db.select("*").from("messages")
-
-		res.json(messages)
-	} catch (err) {
-		next(err)
-	}
+router.get('/', async (req, res, next) => {
+  try {
+    // translates to SELECT * FROM "messages";
+    const messages = await db.select('*').from('messages')
+    res.json(messages)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get("/:id", async (req, res, next) => {
-	try {
-		// translates to `SELECT * FROM "messages" WHERE "id" = ? LIMIT 1;`
-		// descructure the result since we only care about the first index of the array
-		const [message] = await db
-			.select("*")
-			.from("messages")
-			.where("id", req.params.id)
-			// make sure we're only getting a single result,
-			// since we're destructuring the array above
-			.limit(1)
+router.get('/:id', async (req, res, next) => {
+  try {
+    // SELECT * FROM "messages" WHERE "Id" = ?;
+    // destructure the result since we only care about the first index of the array
+    const [message] = await db
+      .select('*')
+      .from('messages')
+      .where('id', req.params.id)
+      // make sure we're only getting a single result
+      // since we're destructuring the array above
+      .limit(1)
 
-		res.json(message)
-	} catch (err) {
-		next(err)
-	}
+    res.json(message)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post("/", async (req, res, next) => {
-	try {
-		const payload = {
-			// the database will automatically generate the ID and dates
-			title: req.body.title,
-			contents: req.body.contents,
-		}
+router.post('/', async (req, res, next) => {
+  try {
+    // INSERT INTO <table> (<columns>) VALUES (<values>);
+    const payload = {
+      // the database will automatically generate the ID and dates
+      title: req.body.title,
+      contents: req.body.contents
+    }
+    const [messageID] = await db
+      .insert(payload)
+      .into('messages')
 
-		// translates to `INSERT INTO "messages" ("title", "contents") VALUES (?, ?);`
-		const [messageID] = await db.insert(payload).into("messages")
-		// calling `.first()` is doing the same thing as `.limit(1)` and destructuring the result
-		const message = await db.first("*").from("messages").where("id", messageID)
+    const message = await db
+      .first('*') // automatically destructures and sends first array item
+      .from('messages')
+      .where('id', messageID)
 
-		res.status(201).json(message)
-	} catch (err) {
-		next(err)
-	}
+    res.status(201).json(message)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.put("/:id", async (req, res, next) => {
-	try {
-		const payload = {
-			title: req.body.title,
-			contents: req.body.contents,
-		}
+router.put('/:id', async (req, res, next) => {
+  try {
+    // UPDATE 'messages' SET ? = ? WHERE "id" = ?;
+    const payload = {
+      // the database will automatically generate the ID and dates
+      title: req.body.title,
+      contents: req.body.contents
+    }
+    await db('messages').update(payload).where('id', req.params.id)
+    const message = await db.first('*').from('messages').where('id', req.params.id)
 
-		// translates to `UPDATE "messages" SET ? = ? WHERE "id" = ?;`
-		await db("messages").update(payload).where("id", req.params.id)
-		const message = await db.first("*").from("messages").where("id", req.params.id)
-
-		res.json(message)
-	} catch (err) {
-		next(err)
-	}
+    res.json(message) // status 200 for updates
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.delete("/:id", async (req, res, next) => {
-	try {
-		// translates to `DELETE FROM "messages" WHERE "id" = ?;`
-		await db("messages").where("id", req.params.id).del()
-		// since we no longer have a resource to return,
-		// just send a 204 which means "success, but no response data is being sent back"
-		res.status(204).end()
-	} catch (err) {
-		next(err)
-	}
+router.delete('/:id', async (req, res, next) => {
+  try {
+    // DELETE FROM 'messages' WHERE "id" = ?;
+    await db('messages').where('id', req.params.id).del()
+    // since we no longer have a resource to return
+    // just send a 204 which means 'success, but no res data is being sent back'
+    res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
